a8efa32910211a57a9234de6ad36bd3b23a91443
Mariusz Brozda <mariuszbrozda.dublin@gmail.com>
2026-06-26 00:28:09 +0100

    Update formatting

--- infra/terraform/modules/eks-cluster/main.tf
+++ infra/terraform/modules/eks-cluster/main.tf
@@ -2,8 +2,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
+  cluster_name                         = var.cluster_name
+  cluster_version                      = var.cluster_version
   vpc_id                               = var.vpc_id
   subnet_ids                           = var.subnet_ids
   cluster_endpoint_private_access      = var.cluster_endpoint_private_access
@@ -11,12 +11,12 @@ module "eks" {
   cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
   control_plane_subnet_ids             = var.control_plane_subnet_ids
 
-  create_kms_key = var.create_kms_key
-  enable_kms_key_rotation = var.enable_kms_key_rotation
+  create_kms_key            = var.create_kms_key
+  enable_kms_key_rotation   = var.enable_kms_key_rotation
   cluster_encryption_config = {}
 
   enable_irsa = var.enable_irsa
-  
+
   eks_managed_node_group_defaults = {
     #  ami_type       = "AL2_x86_64"
     instance_types = ["m5.large"]
@@ -26,12 +26,12 @@ module "eks" {
 
   eks_managed_node_groups = {
     default_node_group = {
-      desired_size   = var.node_desired_capacity
-      max_size       = var.node_max_capacity
-      min_size       = var.node_min_capacity
-      instance_types = var.node_instance_types
+      desired_size               = var.node_desired_capacity
+      max_size                   = var.node_max_capacity
+      min_size                   = var.node_min_capacity
+      instance_types             = var.node_instance_types
       use_custom_launch_template = false
-      disk_size = 50
+      disk_size                  = 50
     }
   }
   tags = {
--- infra/terraform/modules/networking/variables.tf
+++ infra/terraform/modules/networking/variables.tf
@@ -51,13 +51,13 @@ variable "tags" {
 variable "public_subnet_tags" {
   type        = map(string)
   description = "Public subnets Tags"
-  default     = {"kubernetes.io/role/elb" = 1}
+  default     = { "kubernetes.io/role/elb" = 1 }
 }
 
 variable "private_subnet_tags" {
   type        = map(string)
   description = "Private subnets Tags"
-  default     = {"kubernetes.io/role/internal-elb" = 1}
+  default     = { "kubernetes.io/role/internal-elb" = 1 }
 }
 
 
